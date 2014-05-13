d0db9f9dca12545531bc288dca453cc1c5f3c46f
Foster McLane <fkmclane@gmail.com>
2014-05-13 17:09:47 -0400

    Don't complain on clean if files don't exist

--- config.mk
+++ config.mk
@@ -3,13 +3,13 @@ VERSION=0.1
 
 SRC=common.c glsl.c matrix.c object.c opengl.c shader.c texture.c
 
-SRCDIR=src
-DISTDIR=dist
-
 DIRS=bin textures
 DIST=glsl/*.glsl objects/*.obj textures/*.ppm
 
+SRCDIR=src
+DISTDIR=dist
+
 CC?=cc
-CFLAGS+=-std=c99 -pipe -O2 -ggdb
 CPPFLAGS+=-DHAVE_LIBGLEW -DVERSION=$(VERSION)
+CFLAGS+=-std=c99 -pipe -O2 -ggdb
 LDLIBS+=-lm -lGL -lGLEW -lX11 -lXrandr -lXi -lXxf86vm -lXcursor -lpthread -lrt -lglfw3
